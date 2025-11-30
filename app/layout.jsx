75a136dddfa515e122305274e8975538137f7a80
sniffamokgosi-arch <sniffamokgosi@gmail.com>
2025-11-30 18:00:30 +0200

    Add files via upload

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: 'TRACKS ADVENTURE SAFARIS & LODGES'
+};
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
